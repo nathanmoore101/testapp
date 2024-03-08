c44bda97ec3173e5021db4d68d85dd600f7edbe9
Nathan Moore <nathanmoore101@gmail.com>
2024-03-07 22:27:18 -0700

    it works

--- mission9/src/App.tsx
+++ mission9/src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import jsonData from "./CollegeBasketballTeams.json";
+
+const bballTeams = jsonData.teams;
+
+interface teamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return <h1>NCAA College Basketball Teams:</h1>;
+}
+
+class Team extends React.Component<teamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h2>School: {oneTeam.school}</h2>
+        <h3>Mascot: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {bballTeams.map((team, index) => (
+        <div key={index}>
+          <Team {...team} />
+          {index !== bballTeams.length - 1 && <hr />}{" "}
+          {/* Add <hr /> if not the last team */}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
